6ffb25d733a4f66e08a63470ea85c6ec90634305
Franel <chillout9508@gmail.com>
2021-12-16 21:38:21 +0700

    add links on header and footer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
             <div className="nav-wrapper">
                 <a href="#" className="brand-logo">Fortnite shop</a>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
-                    <li><a href="!#">Repo</a></li>
+                    <li><a href="https://github.com/Franel-create/fortnite-shop" target='_blank'>Repo</a></li>
                 </ul>
             </div>
         </nav>
